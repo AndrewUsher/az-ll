90823e9acc3477865988e5ab920c24e25fa0858b
Andrew Usher <andrew.usher@autozone.com>
2020-03-02 15:01:06 -0600

    stopping point

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+export default {
+  styles: {
+    h1: {
+      textTransform: 'uppercase',
+      letterSpacing: '0.1em',
+      fontSize: '6rem',
+      margin: 0
+    },
+    blockquote: {
+      fontStyle: 'italic',
+    },
+    Slide: {
+      // justifyContent: 'flex-start'
+    }
+  }
+}
