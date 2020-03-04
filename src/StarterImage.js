6ef2e2ee942660eb2a01bcf49a9e99e0cf5a53e7
Andrew Usher <andrew.usher@autozone.com>
2020-03-04 10:48:01 -0600

    stopping point

--- src/StarterImage.js
+++ src/StarterImage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const StarterImage = ({src}) => (
-  <img src={src} style={{marginTop: 60,width: '50%'}} />
+const StarterImage = ({ src }) => (
+  <img src={src} alt="" style={{ marginTop: 60, width: '50%' }} />
 )
 
 export { StarterImage }
--- src/theme.js
+++ src/theme.js
@@ -3,17 +3,22 @@ export default {
     h1: {
       textTransform: 'uppercase',
       letterSpacing: '0.1em',
+      lineHeight: 1.4,
       fontSize: '6rem',
       margin: 0
     },
     blockquote: {
-      fontStyle: 'italic',
+      fontStyle: 'italic'
     },
     ul: {
+      maxWidth: '1200px',
       textAlign: 'left'
     },
+    li: {
+      marginBottom: '8px'
+    },
     Slide: {
-      // justifyContent: 'flex-start'
+      padding: 6
     }
   }
 }
